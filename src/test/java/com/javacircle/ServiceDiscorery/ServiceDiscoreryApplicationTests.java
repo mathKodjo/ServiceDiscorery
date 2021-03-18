e4b3d0aebc3b664d247f218b22dcaf901d83dddc
math <mayh.kokjo@gmail.com>
2021-03-18 08:50:13 -0500

    Initial code commit

---
+++ src/test/java/com/javacircle/ServiceDiscorery/ServiceDiscoreryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacircle.ServiceDiscorery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoreryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
